019e5e89e3cb10740c451f43e79e347f107ad0d7
TanksCar <2572813592@qq.com>
2021-08-27 10:03:18 +0800

    Delete useless methods

--- polkadot-wrapper/src/wrapper/contractCall.ts
+++ polkadot-wrapper/src/wrapper/contractCall.ts
@@ -1,22 +1,5 @@
 import { Result } from '../wrapper/actions/result'
 import PredefinedStatus  from '../wrapper/consts/consts'
-import { GasSetting , transactionResultGetter } from '../wrapper/utils' 
-
-function getContractMethod(contract:any,methodName:string,param:any){
-  let method;
-  try{
-    method = contract.methods[methodName]
-  }catch(error){
-    method = null
-  }
-  
-  if(!method) return null
-  let prop = method(...param)._method
-  return {
-    prop:prop,
-    func:method
-  }
-}
 
 class ContractCall {
   public async offChainCall(contract:any,methodName:string,param:[],amount,extra){
@@ -28,4 +11,4 @@ class ContractCall {
   }
   
 }
-export { ContractCall , getContractMethod }
+export { ContractCall }
--- polkadot-wrapper/src/wrapper/utils.ts
+++ polkadot-wrapper/src/wrapper/utils.ts
@@ -5,24 +5,3 @@ const defaultGas={
   price:'4000000000',
   limit:'150000'
 }
-function GasSetting(setting):any{
-  setting.gasPrice = setting.gasPrice ? setting.gasPrice : defaultGas.price
-  setting.gasLimit = setting.gasLimit ? setting.gasLimit : defaultGas.limit
-  return setting
-}
-
-let transactionResultGetter = function (resolve, data , err) {
-  let result
-  console.log(data)
-  if(err) {
-    result = new Result(PredefinedStatus.ERROR_STATE(null))
-  } else {
-    result = new Result(PredefinedStatus.SUCCESS(data))
-  }
-  resolve(result)
-}
-
-export {
-  GasSetting,
-  transactionResultGetter
-}
