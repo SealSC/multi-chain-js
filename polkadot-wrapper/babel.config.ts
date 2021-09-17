fb5c5ef9973f5e3a2200ac08e7f95a53eaa6f7de
TanksCar <2572813592@qq.com>
2021-09-17 10:58:29 +0800

    Add webpack packaging configuration

--- polkadot-wrapper/babel.config.ts
+++ polkadot-wrapper/babel.config.ts
@@ -1,6 +1,11 @@
 module.exports = {
+  "compact": false,
   presets: [
     '@vue/cli-plugin-babel/preset',
-    "@babel/preset-env"
+    ['@babel/preset-env', {'modules': false}],
+    {
+      "corejs": "3",
+      "useBuiltIns": "usage"
+    }
   ]
 }
