e32c10f07331668758c5634f33f4640caabd7f60
lichunyue11 <804111938@qq.com>
2021-09-22 18:49:23 +0800

    solana sdk  Encapsulation2

--- web3-wrapper/test/index.ts
+++
@@ -1,7 +0,0 @@
-const tests = require.context('./', true, /\.spec\.tsx?$/)
-
-tests.keys().forEach(tests)
-
-const sources = require.context('../src/', true, /\.tsx?$/)
-
-sources.keys().forEach(sources)
