4578281997cde25ca235b4c4c264425efbb920cb
TanksCar <30773174+TanksCar@users.noreply.github.com>
2021-09-27 15:07:19 +0800

    Tronweb wrapper (#5)
    * init protocol-project
    
    * Modify some interface type definitions
    
    * Specify the data type of the function return value data
    
    * Add a field that defines the method to return data
    
    * Add the field type within the object
    
    * init web3-wrapper
    
    * A variety of methods return to the data structure defined by the protocol after encapsulation
    
    * Put all methods into actions
    
    * Return the data according to the protocol specification field
    
    * Add the link method to call up the wallet
    
    * Add initialization method
    
    * Add initialization contract and contract call
    
    * Add contract call and initialize contract method
    
    * Build a mocha+karma+chai unit test environment
    
    * Add karma-coverage
    
    * Modified to ts file
    
    * Modified to a test file of ts type
    
    * Add method test code
    
    * Modify the test code and naming errors
    
    * Modify terminal report configuration
    
    * Submit part of the test function
    
    * Submit part of the test code
    
    * Add some methods and method test code
    
    * Modify part of the test method code
    
    * create tronweb-wrapper
    
    * Encapsulation tronweb sdk
    
    * Encapsulation tronweb sdk
    
    * tronweb sdk encapsulation
    
    * tronweb sdk encapsulation
    
    * tronweb sdk
    
    * tronweb sdk
    
    * tronweb sdk
    
    * tronweb sdk
    
    * tronWeb sdk Encapsulation
    
    * tronweb fix
    
    * wallet link
    
    * Modify the status of no wallet
    
    Co-authored-by: lichunyue11 <804111938@qq.com>

---
+++ tronweb-wrapper/src/wrapper/actions/actions/sendTransaction.ts
@@ -0,0 +1,18 @@
+import { Result } from '../result'
+import PredefinedStatus  from '../../consts/consts'
+import TronWeb from 'tronweb'
+class SendTransaction{
+  public async sendTransaction(transactionObject:any){
+    try{
+
+      return  (window as any).isPhantom.trx.sendTransaction(transactionObject.address,transactionObject.amount).then((receipt)=>{
+        return new Result(PredefinedStatus.SUCCESS(receipt))
+      })
+    }catch(err){
+      return new Result(PredefinedStatus.DEFAULT_STATE(err))
+
+    }
+    
+  }
+}
+export { SendTransaction }
---
+++ tronweb-wrapper/src/wrapper/actions/actions/signTransaction.ts
@@ -0,0 +1,11 @@
+import { Result } from '../result'
+import PredefinedStatus  from '../../consts/consts'
+// import {signTransactionField } from '../../fieldDefinition/signTransactionField'
+
+
+class SignTransaction{
+  public async signTransaction(dataToSign:object|string,address:string){
+    return new Result(PredefinedStatus.NOT_SUPPORT(null))
+  }
+}
+export { SignTransaction }
