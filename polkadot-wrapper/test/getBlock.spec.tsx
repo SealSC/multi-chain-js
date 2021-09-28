18378bc0b320dc104b0b48f46460966a844bf330
TanksCar <2572813592@qq.com>
2021-09-28 12:09:41 +0800

    Modify the return field of the getblock method

--- polkadot-wrapper/test/getBlock.spec.tsx
+++ polkadot-wrapper/test/getBlock.spec.tsx
@@ -11,10 +11,29 @@ describe('getBlock', () => {
     const api = await ApiPromise.create({ provider: wsProvider });
     const blockHash = await api.rpc.chain.getBlockHash(6521853);
     const signedBlock = await api.rpc.chain.getBlock(blockHash);
-    let Block = JSON.parse(`${signedBlock.block.header}`)
+    let BlockInfo = JSON.parse(`${signedBlock.block.header}`)
+    let blockDataField = new getBlockField()
+    blockDataField.number = BlockInfo.number
+    blockDataField.hash = null
+    blockDataField.parentHash = BlockInfo.parentHash
+    blockDataField.nonce = null
+    blockDataField.sha3Uncles = null
+    blockDataField.logsBloom = null
+    blockDataField.transactionsRoot = null
+    blockDataField.stateRoot = BlockInfo.stateRoot
+    blockDataField.miner = null
+    blockDataField.difficulty =null
+    blockDataField.totalDifficulty = null
+    blockDataField.size = null
+    blockDataField.extraData = null
+    blockDataField.gasLimit = null
+    blockDataField.gasUsed = null
+    blockDataField.timestamp = null
+    blockDataField.transactions = null
+    blockDataField.uncles = null
     let Action = await new Actions()
     let shouldBlockHash= await Action.actions.getBlock(6521853)
-    let results = new Result(PredefinedStatus.SUCCESS(Block))
+    let results = new Result(PredefinedStatus.SUCCESS(blockDataField))
     expect(shouldBlockHash).to.deep.equal(results)
   }).timeout(100000)
 
