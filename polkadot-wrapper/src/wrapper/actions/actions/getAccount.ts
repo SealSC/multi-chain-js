061ee578348e26d56544fb63ad3ac616a7f14eb2
TanksCar <2572813592@qq.com>
2021-10-09 18:21:34 +0800

    Modify getAccount

--- polkadot-wrapper/src/wrapper/actions/actions/getAccount.ts
+++ polkadot-wrapper/src/wrapper/actions/actions/getAccount.ts
@@ -1,20 +1,16 @@
 import { Result } from '../result'
-import PredefinedStatus  from '../../consts/consts'
-import {getAccountInterface } from '../classInterface'
-import { web3Accounts, web3Enable ,web3FromSource } from '@polkadot/extension-dapp';
+import PredefinedStatus from '../../consts/consts'
+import { getAccountInterface } from '../classInterface'
+import { web3Accounts, web3Enable, web3FromSource } from '@polkadot/extension-dapp';
 
-class GetAccount{
-  public async getAccount(){
+class GetAccount {
+  public async getAccount() {
     const allAccounts = await web3Accounts();
-<<<<<<< HEAD
-    let Account :any[] = []
-    for(let i=0;i<allAccounts.length;i++){
+    let Account: any[] = []
+    for (let i = 0; i < allAccounts.length; i++) {
       Account.push(allAccounts[i].address)
     }
     return new Result(PredefinedStatus.SUCCESS(Account))
-=======
-    return new Result(PredefinedStatus.SUCCESS(allAccounts))
->>>>>>> main
   }
 }
 export { GetAccount }
