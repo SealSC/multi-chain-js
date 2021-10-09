abfe117c11fa3f01066e9205918d218bfc8f0f00
TanksCar <30773174+TanksCar@users.noreply.github.com>
2021-10-09 20:36:02 +0800

    Submit project descriptions and some method modifications

--- web3-wrapper/src/wrapper/contractCall.ts
+++ web3-wrapper/src/wrapper/contractCall.ts
@@ -1,66 +1,74 @@
 import { Result } from '../wrapper/actions/result'
-import PredefinedStatus  from '../wrapper/consts/consts'
-import { GasSetting , transactionResultGetter } from '../wrapper/utils' 
+import PredefinedStatus from '../wrapper/consts/consts'
+import { GasSetting, transactionResultGetter } from '../wrapper/utils'
 
-function getContractMethod(contract:any,methodName:string,param:any){
+function getContractMethod(contract: any, methodName: string, param: any) {
   let method;
-  try{
+  try {
     method = contract.methods[methodName]
-  }catch(error){
+  } catch (error) {
     method = null
   }
-  
-  if(!method) return null
+
+  if (!method) return null
   let prop = method(...param)._method
   return {
-    prop:prop,
-    func:method
+    prop: prop,
+    func: method
   }
 }
 
 class ContractCall {
-  public async offChainCall(contract:any,methodName:string,param:[],amount,extra){
-    let account = Object.keys((window as any).web3).length !== 0 ? await (window as any).web3.eth.getAccounts():[''];
-    let method = getContractMethod(contract,methodName,param)
-   
-    if(!method){
+  public async loadContract(abi: any, address: string) {
+    try {
+      let Contract: any = await new (window as any).web3.eth.Contract(abi, address)
+      return new Result(PredefinedStatus.SUCCESS(Contract))
+    } catch (err) {
+      return new Result(PredefinedStatus.ERROR_STATE(null))
+    }
+  }
+  public async offChainCall(contract: any, methodName: string, param: [], amount, extra) {
+    let account = Object.keys((window as any).web3).length !== 0 ? await (window as any).web3.eth.getAccounts() : [''];
+    let method = getContractMethod(contract, methodName, param)
+
+    if (!method) {
       return new Result(PredefinedStatus.ERROR_STATE('参数有误'))
     }
-    return await new Promise(async res=>{
-      method!.func(...param).call({from:account[0],gas:'100000000'},(err,result)=>{
+    return await new Promise(async res => {
+      method!.func(...param).call({ from: account[0], gas: '100000000' }, (err, result) => {
         transactionResultGetter(res, result, err)
-      }).catch(reason=>{
+      }).catch(reason => {
         console.log(2)
         transactionResultGetter(res, reason, null)
       })
     })
   }
 
-  public async onChainCall(contract:any,methodName:string,param:any,extra:any){
- 
-    let account = Object.keys((window as any).web3).length !== 0 ? await (window as any).web3.eth.getAccounts():[''];
+  public async onChainCall(contract: any, methodName: string, param: any, extra: any) {
+
+    let account = Object.keys((window as any).web3).length !== 0 ? await (window as any).web3.eth.getAccounts() : [''];
     console.log(account)
-  
-    let method = getContractMethod(contract,methodName,param)
-    if(!method){
+
+    let method = getContractMethod(contract, methodName, param)
+    if (!method) {
       return new Result(PredefinedStatus.ERROR_STATE('参数有误'))
     }
     let gasSetting = GasSetting(extra)
-    let sendParam={
-      from:account[0],
-      gasPrice:gasSetting.gasPrice,
-      gas:gasSetting.gasLimit
+    let sendParam = {
+      from: account[0],
+      gasPrice: gasSetting.gasPrice,
+      gas: gasSetting.gasLimit
     }
-    return await new Promise(res=>{
-      method!.func(...param).send(sendParam,(err,tx)=>{
+    return await new Promise(res => {
+      method!.func(...param).send(sendParam, (err, tx) => {
         // transactionResultGetter(res, err, tx)
-      }).then(receipt=>{
+      }).then(receipt => {
         transactionResultGetter(res, receipt, null)
-      }).catch(reason=>{
+      }).catch(reason => {
         transactionResultGetter(res, reason, null)
       })
     })
   }
-  
+
 }
-export { ContractCall , getContractMethod }
+export { ContractCall, getContractMethod }
--- web3-wrapper/test/getContractMethod.spec.tsx
+++ web3-wrapper/test/getContractMethod.spec.tsx
@@ -1,26 +1,26 @@
 import { Actions } from '../src/wrapper/wrapper'
 import { Result } from '../src/wrapper/actions/result'
-import PredefinedStatus  from '../src/wrapper/consts/consts'
+import PredefinedStatus from '../src/wrapper/consts/consts'
 import { web3Install } from '../src/wrapper/web3'
-import { getContractMethod } from '../src/wrapper/contractCall' 
+import { getContractMethod } from '../src/wrapper/contractCall'
 let ERC20 = require('../src/abis/ERC20-ABI.json')
 
 describe('getContractMethod', () => {
-  it('Wallet installed getContractMethod', async ()=>{
+  it('Wallet installed getContractMethod', async () => {
 
     (window as any).web3 = await web3Install()
     let Action = await new Actions()
-    let ContractIn = await Action.actions.loadContract(ERC20,'0xf1a249C0675444A989d281dfE262D370AcCa8774')  
-    let ContractMethod = await getContractMethod(ContractIn.data,'decimals',[])
+    let ContractIn = await Action.contract.loadContract(ERC20, '0xf1a249C0675444A989d281dfE262D370AcCa8774')
+    let ContractMethod = await getContractMethod(ContractIn.data, 'decimals', [])
     expect(ContractMethod).to.not.null
 
   }).timeout(100000)
 
-  it('Wallet not installed getContractMethod',async ()=>{
+  it('Wallet not installed getContractMethod', async () => {
     let Action = await new Actions()
-    let ContractIn = await Action.actions.loadContract(ERC20,'0xf1a249C0675444A989d281dfE262D370AcCa8774')  
-    let ContractMethod = await getContractMethod(ContractIn.data,'names',[])
+    let ContractIn = await Action.contract.loadContract(ERC20, '0xf1a249C0675444A989d281dfE262D370AcCa8774')
+    let ContractMethod = await getContractMethod(ContractIn.data, 'names', [])
     expect(ContractMethod).to.be.null
-  }).timeout(100000) 
+  }).timeout(100000)
 
 })
