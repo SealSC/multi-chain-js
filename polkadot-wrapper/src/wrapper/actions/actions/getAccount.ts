4d7f59f038e1a2b9c5b36aeac40483f0242c929c
TanksCar <30773174+TanksCar@users.noreply.github.com>
2021-10-09 20:31:36 +0800

    Polkadot wrapper (#12)
    Modify some project descriptions and methods

--- polkadot-wrapper/src/wrapper/actions/actions/getAccount.ts
+++ polkadot-wrapper/src/wrapper/actions/actions/getAccount.ts
@@ -1,12 +1,16 @@
 import { Result } from '../result'
-import PredefinedStatus  from '../../consts/consts'
-import {getAccountInterface } from '../classInterface'
-import { web3Accounts, web3Enable ,web3FromSource } from '@polkadot/extension-dapp';
+import PredefinedStatus from '../../consts/consts'
+import { getAccountInterface } from '../classInterface'
+import { web3Accounts, web3Enable, web3FromSource } from '@polkadot/extension-dapp';
 
-class GetAccount{
-  public async getAccount(){
+class GetAccount {
+  public async getAccount() {
     const allAccounts = await web3Accounts();
-    return new Result(PredefinedStatus.SUCCESS(allAccounts))
+    let Account: any[] = []
+    for (let i = 0; i < allAccounts.length; i++) {
+      Account.push(allAccounts[i].address)
+    }
+    return new Result(PredefinedStatus.SUCCESS(Account))
   }
 }
 export { GetAccount }
