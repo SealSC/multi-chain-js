ac7ab756b78aabd65aafef377b9772c751735ecb
lichunyue11 <804111938@qq.com>
2021-10-27 17:02:52 +0800

    change

--- polkadot-wrapper/src/wrapper/actions/actions/getAccount.ts
+++ polkadot-wrapper/src/wrapper/actions/actions/getAccount.ts
@@ -2,10 +2,10 @@ import { Result } from '../result'
 import PredefinedStatus from '../../consts/consts'
 import { getAccountInterface } from '../classInterface'
 import { web3Accounts, web3Enable, web3FromSource } from '@polkadot/extension-dapp';
-
 class GetAccount {
   public async getAccount() {
     const allAccounts = await web3Accounts();
+    console.log(allAccounts,"allAccounts")
     let Account: any[] = []
     for (let i = 0; i < allAccounts.length; i++) {
       Account.push(allAccounts[i].address)
--- tronweb-wrapper/karma.conf.js
+++ tronweb-wrapper/karma.conf.js
@@ -15,9 +15,9 @@ module.exports = config => {
     mime: {
       'text/x-typescript': ['ts', 'tsx']
     },
-    colors:true,
-    reporters: ['spec', 'coverage-istanbul','coveralls'],
-    browsers: process.env.CI ? ['Chrome', 'Firefox'] : ['Chrome'],
+    colors: true,
+    reporters: ['spec', 'coverage-istanbul', 'coveralls'],
+    browsers: [],
 
     webpackMiddleware: {
       noInfo: true,
@@ -25,7 +25,7 @@ module.exports = config => {
     },
     webpack: {
       mode: 'development',
-      entry: './get-selection-more.ts',
+      entry: './src/wrapper/wrapper.ts',
       output: {
         filename: '[name].js'
       },
@@ -60,19 +60,19 @@ module.exports = config => {
 
     coverageIstanbulReporter: process.env.CI
       ? {
-          reports: ['lcovonly', 'text-summary'],
-          dir: path.join(__dirname, 'coverage'),
-          combineBrowserReports: true,
-          fixWebpackSourcePaths: true
-        }
+        reports: ['lcovonly', 'text-summary'],
+        dir: path.join(__dirname, 'coverage'),
+        combineBrowserReports: true,
+        fixWebpackSourcePaths: true
+      }
       : {
-          reports: ['html', 'lcovonly', 'text-summary'],
-          dir: path.join(__dirname, 'coverage/%browser%/'),
-          fixWebpackSourcePaths: true,
-          'report-config': {
-            html: { outdir: 'html' }
-          }
-        },
+        reports: ['html', 'lcovonly', 'text-summary'],
+        dir: path.join(__dirname, 'coverage/%browser%/'),
+        fixWebpackSourcePaths: true,
+        'report-config': {
+          html: { outdir: 'html' }
+        }
+      },
 
     coverageReporter: {
       type: 'lcovonly',
