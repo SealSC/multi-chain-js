2df21556dcdcf1b87a6b5ab5bbbf8a178c7e1d37
TanksCar <2572813592@qq.com>
2021-11-01 20:58:26 +0800

    Modify the test process method in the absence of a wallet

--- polkadot-wrapper/src/wrapper/actions/actions/sendSignedTransaction.ts
+++ polkadot-wrapper/src/wrapper/actions/actions/sendSignedTransaction.ts
@@ -6,26 +6,30 @@ import Decimal from 'decimal.js'
 
 class SendSignedTransaction{
   public async sendSignedTransaction(address:string,count:string|number){
-    
-    const allAccounts = await web3Accounts();
-    const account = allAccounts[0];
-    let counts = await new Decimal(count).mul(new Decimal(Math.pow(10,10))).toString()
-    const transferExtrinsic = (window as any).api.tx.balances.transfer(address, counts)
-    const injector = await web3FromSource(account.meta.source);
-    const signer = await injector.signer
-    return new Promise((resolve,reject)=>{
-      transferExtrinsic.signAndSend(account.address, { signer: signer }, ({ status }) => {
-        if (status.isInBlock) {
-          console.log(`Completed at block hash #${status.asInBlock.toString()}`);
-          resolve(new Result(PredefinedStatus.SUCCESS(status.asInBlock.toString())))
-        } else {
-          console.log(`Current status: ${status.type}`);
-        }
-      }).catch((error: any) => {
-          console.log(':( transaction failed', error);
-          reject(new Result(PredefinedStatus.ERROR_STATE(null)))
-      });
-    })
+    try{
+      const allAccounts = await web3Accounts();
+      const account = allAccounts[0];
+      let counts = await new Decimal(count).mul(new Decimal(Math.pow(10,10))).toString()
+      const transferExtrinsic = (window as any).api.tx.balances.transfer(address, counts)
+      const injector = await web3FromSource(account.meta.source);
+      const signer = await injector.signer
+      return new Promise((resolve,reject)=>{
+        transferExtrinsic.signAndSend(account.address, { signer: signer }, ({ status }) => {
+          if (status.isInBlock) {
+            console.log(`Completed at block hash #${status.asInBlock.toString()}`);
+            resolve(new Result(PredefinedStatus.SUCCESS(status.asInBlock.toString())))
+          } else {
+            console.log(`Current status: ${status.type}`);
+          }
+        }).catch((error: any) => {
+            console.log(':( transaction failed', error);
+            reject(new Result(PredefinedStatus.ERROR_STATE(null)))
+        });
+      })
+    }catch(error){
+      return new Result(PredefinedStatus.ERROR_STATE(null));
+    }
+   
   }
 }
 export { SendSignedTransaction }
