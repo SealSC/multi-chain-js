cf9f6321c61b13b3ca3fc5507331dc0776f66486
TanksCar <2572813592@qq.com>
2021-11-01 14:34:48 +0800

    Modify the test problem of signWithWallet module

--- web3-wrapper/test/signWithWallet.spec.tsx
+++ web3-wrapper/test/signWithWallet.spec.tsx
@@ -10,8 +10,9 @@ describe('SignWithWallet', () => {
 
     (window as any).web3 = await web3Install()
     let Action = await new Actions();
-    let shouldSign = await Action.actions.signWithWallet('2','0x5B6C6709d1000db91252c8c6E84B8987D1D10829');
-    let sign = await (window as any).web3.eth.personal.sign((window as any).web3.utils.utf8ToHex("2"),'0x5B6C6709d1000db91252c8c6E84B8987D1D10829');
+    let accounts = await (window as any).web3.eth.getAccounts()
+    let shouldSign = await Action.actions.signWithWallet('2',accounts[0]);
+    let sign = await (window as any).web3.eth.personal.sign((window as any).web3.utils.utf8ToHex("2"),accounts[0]);
     let results = new Result(PredefinedStatus.SUCCESS(sign));
     expect(shouldSign).to.deep.equal(results)
 
@@ -21,6 +22,7 @@ describe('SignWithWallet', () => {
 
     (window as any).web3 = {}
     let Action = await new Actions();
+    
     let shouldGetTransactionCount = await Action.actions.signWithWallet('2','0x5B6C6709d1000db91252c8c6E84B8987D1D10829');
     
     let results = new Result(PredefinedStatus.ERROR_STATE(null));
