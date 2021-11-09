2240740d5eecb723d93bbd81e70ddb051864af1c
TanksCar <2572813592@qq.com>
2021-11-09 13:56:55 +0800

    Add processing after transaction failure

--- polkadot-wrapper/test/sendSignedTransaction.spec.tsx
+++ polkadot-wrapper/test/sendSignedTransaction.spec.tsx
@@ -1,15 +1,18 @@
 import { Actions } from '../src/wrapper/wrapper'
 import { Result } from '../src/wrapper/actions/result'
 import PredefinedStatus  from '../src/wrapper/consts/consts'
-import {ApiPromise, WsProvider} from '@polkadot/api'
 
 describe('sendSignedTransaction', () => {
 
   it('sendSignedTransaction', async ()=>{
     try{
       let Action = await new Actions()
-      let sendSignedTransaction:any = await Action.actions.sendSignedTransaction('14EHXD3oats4uQQ3Rp6DByaiQzowt5ep4R5fXCpMkkF3oDps',0.1)
-      expect(sendSignedTransaction.code).to.equal(0)
+      Action.actions.sendSignedTransaction('14EHXD3oats4uQQ3Rp6DByaiQzowt5ep4R5fXCpMkkF3oDps',0.1).then((resolve :any)=>{
+        expect(resolve.code).to.equal(0)
+      }).catch((reject)=>{
+        let results = new Result(PredefinedStatus.ERROR_STATE(null));
+        expect(reject).to.deep.equal(results)
+      })
     }catch(error){
       let Action = await new Actions()
       let sendSignedTransaction:any = await Action.actions.sendSignedTransaction('14EHXD3oats4uQQ3Rp6DByaiQzowt5ep4R5fXCpMkkF3oDps',0.1)
